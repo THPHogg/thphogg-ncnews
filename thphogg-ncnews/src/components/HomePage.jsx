08662a704ea74e6d7f737cd184db5babd48d5e58
THPHogg <t.hogg@hotmail.com>
2020-12-09 16:51:33 +0000

    added more css to make it responsive for mobile users

--- thphogg-ncnews/src/components/HomePage.jsx
+++ thphogg-ncnews/src/components/HomePage.jsx
@@ -3,8 +3,39 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h3>This is the homepage to Northcoders News!</h3>
-      <p>Take your time and have a look around!</p>
+      <h3>Welcome to Northcoders News!</h3>
+      <p>The home of all things coding, cooking and footy!</p>
+      <container>
+        <section className="codingSection">
+          <h3>Coding</h3>
+          <section className="homeCard">
+            <p>
+              Here is a little bit of information on the great articles you can
+              find waiting for you:
+            </p>
+          </section>
+        </section>
+        <section className="cookingSection">
+          <h3>Cooking</h3>
+          <section className="homeCard">
+            {' '}
+            <p>
+              Here is a little bit of information on the great articles you can
+              find waiting for you:
+            </p>
+          </section>
+        </section>
+        <section className="footySection">
+          <h3>Footy</h3>
+          <section className="homeCard">
+            {' '}
+            <p>
+              Here is a little bit of information on the great articles you can
+              find waiting for you:
+            </p>
+          </section>
+        </section>
+      </container>
     </div>
   );
 };
