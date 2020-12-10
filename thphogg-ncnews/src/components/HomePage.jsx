e5233629d3f025967e4ee862f8bb20bf1ae62b36
THPHogg <t.hogg@hotmail.com>
2020-12-10 16:04:32 +0000

    slightly altered hommepage css

--- thphogg-ncnews/src/components/HomePage.jsx
+++ thphogg-ncnews/src/components/HomePage.jsx
@@ -31,77 +31,77 @@ class HomePage extends Component {
       <div>
         <h3>Welcome to Northcoders News!</h3>
         <p>The home of all things coding, cooking and footy!</p>
-        <section className="codingSection">
-          <h3>Login:</h3>
-          <section className="homeCard">
-            {!loggedInUser ? (
-              <div>
-                <p>
-                  Please login below to unlock features such as posting,
-                  deleting and altering your comments and articles!
-                </p>
-                <br></br>
-                <form>
-                  <label>
-                    Username:<br></br>
-                    <input
-                      type="text"
-                      name="username"
-                      id="username"
-                      onChange={(event) => this.handleChange(event)}
-                    ></input>
-                  </label>
+        <div className="outerBox">
+          <section>
+            <h3>Login:</h3>
+            <section className="titleCard">
+              {!loggedInUser ? (
+                <div>
+                  <p>
+                    Please login below to unlock features such as posting,
+                    deleting and altering your comments and articles!
+                  </p>
                   <br></br>
+                  <form>
+                    <label>
+                      Username:<br></br>
+                      <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        onChange={(event) => this.handleChange(event)}
+                      ></input>
+                    </label>
+                    <br></br>
+                    <br></br>
+                    <label>
+                      Password:<br></br>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                      ></input>
+                    </label>
+                    <br></br>
+                    <br></br>
+                    <button onClick={() => logIn(user)}>Login</button>
+                  </form>
+                </div>
+              ) : (
+                <div>
+                  <p>
+                    Welcome <b>{loggedInUser}</b>, you are now able to post
+                    delete and alter your comments and articles!
+                  </p>
                   <br></br>
-                  <label>
-                    Password:<br></br>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                    ></input>
-                  </label>
-                  <br></br>
-                  <br></br>
-                  <button onClick={() => logIn(user)}>Login</button>
-                </form>
-              </div>
-            ) : (
-              <div>
-                <p>
-                  Welcome {loggedInUser}, you are now able to post delete and
-                  alter your comments and articles!
-                </p>
-                <br></br>
-                <button onClick={() => logOut()}>Log Out</button>
-              </div>
-            )}
-          </section>
-        </section>
-        <container>
-          <section className="cookingSection">
-            <h3>Top Story:</h3>
-            <section className="homeCard">
-              <h3>{topArticle.title}</h3>
-              <b>
-                <p>By {topArticle.author}</p>
-              </b>
-              <br></br>
-              <p>{topArticle.body}</p>
+                  <button onClick={() => logOut()}>Log Out</button>
+                </div>
+              )}
             </section>
           </section>
-          <section className="footySection">
-            <h3>Newest Story:</h3>
-            <section className="homeCard">
-              <h3>{newArticle.title}</h3>
-              <b>
-                <p>By {newArticle.author}</p>
-              </b>
-              <br></br>
-              <p>{newArticle.body}</p>
+          <section className="lowerSection">
+            <section className="topStorySection">
+              <h3>Top Story:</h3>
+              <section className="topStoryCard">
+                <h3>{topArticle.title}</h3>
+                <b>
+                  <p>By {topArticle.author}</p>
+                </b>
+                <p>{topArticle.body}</p>
+              </section>
+            </section>
+            <section className="newStorySection">
+              <h3>Newest Story:</h3>
+              <section className="newStoryCard">
+                <h3>{newArticle.title}</h3>
+                <b>
+                  <p>By {newArticle.author}</p>
+                </b>
+                <p>{newArticle.body}</p>
+              </section>
             </section>
           </section>
-        </container>
+        </div>
       </div>
     );
   }
